ed60f9e23575122977ecd2464d546e28743b7d47
t7pash00 <t7pash00@students.oamk.fi>
2018-12-02 20:55:19 +0200

    "TG-16 #READY-FOR-TEST"

--- mobileshop-client/src/Components/Phones/Displayphones.js
+++ mobileshop-client/src/Components/Phones/Displayphones.js
@@ -9,36 +9,33 @@ class Displayphones extends Component {
   constructor() {
     super();
     this.showPhones = this.showPhones.bind(this);
-    this.showdetail = this.showdetail.bind(this);
     this.state = {
 
       phones: []
     };
+    this.showPhones();
   }
   showPhones() {
     axios.get("http://localhost:3000/phones").then(res => {
       const phones = res.data;
       this.setState({ phones });
     });
+
+
   }
-showdetail()
-{
-  alert("sdkjgksejg");
-  
-}
   render() {
     return (
       <div className="main-wrap "  >
         <div class="container-fluid" style={{ marginTop: '80px' }}>
           <div>
-            {this.onload = this.showPhones()}
+
             <div class="row">
               {this.state.phones.map(phones => (
                 <div class="col-md-4 mt-3" key={phones.phone_id}>
 
                   <div class="card profile-card-5">
                     <div class="card-img-block">
-                      <img class="card-img-top" style={{ width: "400px", height: "400px" }} src={`http://localhost:3001/Assets/Images/${phones.imagename}`} />
+                      <img class="card-img-top" style={{ width: "250px", height: "350px" }} src={`http://localhost:3001/Assets/Images/${phones.imagename}`} />
                     </div>
                     <div class="card-body pt-0">
                       <h5 class="card-title">{phones.brand_name}</h5>
