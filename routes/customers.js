41528a749985f295ccd75744819f671f62b541a4
shyamraja <t7bhsh00@students.oamk.fi>
2018-12-03 12:04:49 +0200

    changes made to routes in customer

--- routes/customers.js
+++ routes/customers.js
@@ -2,28 +2,30 @@ var express = require('express');
 var router = express.Router();
 var customers = require('../models/customers');
 
-router.get('/Login:customer_email?', function(req, res, next) {
-  if (req.params.customer_email) {
-    customers.getcustomerByEmailPass(req.params.customer_email, 
-      function(err,rows ) {
-      if (err) {
-        res.json(err);
-      } else {
-        res.json(rows);
-      }
-    });
-  
 
-router.get('/Login:customer_password?', function(req, res, next) {
-  if (req.params.customer_password) {
-    customers.getcustomerByEmailPass(req.params.customer_password, 
-      function(err,rows ) {
-      if (err) {
-        res.json(err);
-      } else {
-        res.json(rows);
-      }
-    });
+
+  router.get('/Login:customer_email?', function(req, res, next){
+
+  customers.getcustomerByEmailPass(req.params.customer_email, function(err,rows ) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows); 
+    }
+  });
+});
+router.get('/Login:customer_password?', function(req, res, next){
+
+  customers.getcustomerBypassword(req.params.customer_password, function(err,rows ) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows); 
+    }
+  });
+});
+
+
 router.get('/:customer_id?', function(req, res, next) {
   if (req.params.customer_id) {
     customers.getcustomerBycustomer_id(req.params.customer_id, 
@@ -55,6 +57,7 @@ router.post('/',
     }
   });
 });
+
 router.delete('/:customer_id',
  function(req, res, next) {
   customers.deletecustomer(req.params.customer_id, 
@@ -81,4 +84,3 @@ function(req, res, next) {
   });
 });
 module.exports = router;
-
