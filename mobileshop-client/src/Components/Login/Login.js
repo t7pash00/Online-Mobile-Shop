8664fda4038f8f3b3cc784037a35aee99d10a41b
shyamraja <t7bhsh00@students.oamk.fi>
2018-12-04 15:34:08 +0200

    changes made to login.js

--- mobileshop-client/src/Components/Login/Login.js
+++ mobileshop-client/src/Components/Login/Login.js
@@ -22,60 +22,90 @@ class Loginmodal extends Component {
   }
 
   onChange = e => {
+  
     const state = this.state;
     state[e.target.name] = e.target.value;
-    this.setState(state);
+    this.setState({
+    state });
   };
+
 
   handleSubmit = event => {
-    event.preventDefault();
 
     const { customer_id, firstname, lastname, email, password, phone, streetAddress, city, postalCode } = this.state;
-
-    axios
-      .post('http://localhost:3000/customers', { customer_id, firstname, lastname, email, password, phone, streetAddress, city, postalCode })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      });
-  };
+//console.log("handleSubmit, event", event);
+this.handleSubmit(event)
+  var apiBaseUrl = "http://localhost:3000/customers";
+  var self = this;
+  var payload={
+  "email":this.state.email,
+  "password":this.state.password
+  }
+  axios.post(apiBaseUrl+'login', payload)
+  .then(function (response) {
+  console.log(response);
+  if(response.data.code == 200){
+  console.log("Login successfull");
+  
+  }
+  else if(response.data.code == 204){
+  console.log("Username password do not match");
+  alert("username password do not match")
+  }
+  else{
+  console.log("Username does not exists");
+  alert("Username does not exist");
+  }
+  })
+  .catch(function (error) {
+  console.log(error);
+  });
+  }
+ axios
+    //   .post('http://localhost:3000/customers', {customer_id, firstname, lastname, email, password, phone, streetAddress, city, postalCode })
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   });
+  
 
   render() {
     return (
-      
+
+  
                             <form onSubmit={this.handleSubmit}>
                             
                            
                           
-                                <div id="loginpage" class="modal fade" role="dialog">
-                                  <div class="modal-dialog">
+                                <div id="loginpage" className="modal fade" role="dialog">
+                                  <div className="modal-dialog">
                                 
                                   
-                                    <div class="modal-content">
-                                      <div class="modal-header">
-                                          <h4 class="modal-title">Login</h4>
-                                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <div className="modal-content">
+                                      <div className="modal-header">
+                                          <h4 className="modal-title">Login</h4>
+                                          <button type="button" className="close" data-dismiss="modal">&times;</button>
                                       </div>
-                                      <div class="modal-body">
+                                      <div className="modal-body">
                                   
-                                      <div class="form-group row">
-                                                  <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-sm">Email :</label>
-                                                <div class="col-sm-7">
-                                                    <input type="text" class="form-control form-control-sm"  placeholder="email" onChange={this.onChange}/>
+                                      <div className="form-group row">
+                                                  <label for="colFormLabelSm" className="col-sm-3 col-form-label col-form-label-sm">Email :</label>
+                                                <div className="col-sm-7">
+                                                    <input type="text" className="form-control form-control-sm"  placeholder="email" onChange={this.onChange}/>
                                                 </div>
                                               </div>
                                           
-                                              <div class="form-group row">
-                                                  <label for="colFormLabelSm" class="col-sm-3 col-form-label col-form-label-sm">Password :</label>
-                                                <div class="col-sm-7">
-                                                    <input type="password" class="form-control form-control-sm"  placeholder="password" onChange={this.onChange}/>
+                                              <div className="form-group row">
+                                                  <label for="colFormLabelSm" className="col-sm-3 col-form-label col-form-label-sm">Password :</label>
+                                                <div className="col-sm-7">
+                                                    <input type="password" className="form-control form-control-sm"  placeholder="password" onChange={this.onChange}/>
                                                 </div>
                                               </div>
                                              
                                               </div>
 
-                                            <div class="modal-footer">
-                                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <div className="modal-footer">
+                                              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
                                               <button className="btn btn-success" type="submit">Login</button>
                                               
                                               </div>
